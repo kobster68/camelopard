bba3d5605f189210e46d737a95c9d5d231e5cb49
Kobe LaPrade <41244097+kobster68@users.noreply.github.com>
2026-06-07 23:51:44 -0400

    ref: remove template logger code

--- src/main/java/com/kobster68/camelopard/Camelopard.java
+++ src/main/java/com/kobster68/camelopard/Camelopard.java
@@ -1,11 +1,9 @@
 package com.kobster68.camelopard;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Camelopard.MODID, name = Camelopard.NAME)
 public class Camelopard
@@ -13,18 +11,15 @@ public class Camelopard
     public static final String MODID = "camelopard";
     public static final String NAME = "Camelopard";
 
-    private static Logger logger;
-
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-        logger = event.getModLog();
+
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+
     }
 }
