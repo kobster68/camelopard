1d4ccb355351c07370268703261d4a2f7e0a8129
Kobe LaPrade <41244097+kobster68@users.noreply.github.com>
2026-06-08 00:02:44 -0400

    feat: register mixed seeds item

--- src/main/java/com/kobster68/camelopard/init/ItemsCamelopard.java
+++ src/main/java/com/kobster68/camelopard/init/ItemsCamelopard.java
@@ -1,5 +1,20 @@
 package com.kobster68.camelopard.init;
 
-public class ItemsCamelopard {
+import com.kobster68.camelopard.Camelopard;
+import com.kobster68.camelopard.items.ItemMixedSeeds;
 
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.item.Item;
+
+@Mod.EventBusSubscriber(modid = Camelopard.MODID)
+public class ItemsCamelopard {
+	
+	public static final Item MIXED_SEEDS = new ItemMixedSeeds();
+	
+	@SubscribeEvent
+	public static void registerItems(RegistryEvent.Register<Item> event) {
+		event.getRegistry().register(MIXED_SEEDS);
+	}
 }
