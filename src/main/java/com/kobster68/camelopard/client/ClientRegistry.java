3a739e00710db662c51b2fa19dd93e7a394b48ad
Kobe LaPrade <41244097+kobster68@users.noreply.github.com>
2026-06-10 23:30:48 -0400

    feat: add registry event for items

--- src/main/java/com/kobster68/camelopard/client/ClientRegistry.java
+++ src/main/java/com/kobster68/camelopard/client/ClientRegistry.java
@@ -1,5 +1,30 @@
 package com.kobster68.camelopard.client;
 
+import com.kobster68.camelopard.Camelopard;
+import com.kobster68.camelopard.init.ItemsCamelopard;
+
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+
+@Mod.EventBusSubscriber(modid = Camelopard.MODID, value = Side.CLIENT)
 public class ClientRegistry {
 
+	@SubscribeEvent
+	public static void registerModels(ModelRegistryEvent event) {
+		registerItemModel(ItemsCamelopard.MIXED_SEEDS);
+	}
+	
+	private static void registerItemModel(Item item) {
+		ModelLoader.setCustomModelResourceLocation(
+				item, 
+				0, 
+				new ModelResourceLocation(item.getRegistryName(), "inventory")
+				);
+	}
+	
 }
